05014ea66eee1486b36066c100c965cc8af1d07e
Liz Ardolino <lizardolino@Lizs-MacBook-Pro.local>
2018-04-20 16:00:02 +0100

    some styling added to User

--- src/components/User.js
+++ src/components/User.js
@@ -2,40 +2,22 @@ import React, { Component } from "react";
 
 class User extends Component {
 
-
     render() {
         const {users, username} = this.props;
-        console.log(users)
         const user = users.filter(user => user.username === username)[0]
         if(!user) return null
         return (
-//             <div class="card" style="width: 18rem;">
-//   <img class="card-img-top" src="{user.avatar_url}" alt="Card image cap"/>
-//   <div class="card-body">
-//     <h5 class="card-title">Card title</h5>
-//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//     <a href="#" class="btn btn-primary">Go somewhere</a>
-//   </div>
-// </div>
+            <div className="card user-card" >
+            <h5 className="card-title">{user.name}</h5>
+            <p className="card-text">{user.username}</p>
+  <img className="card-img-top card-image-size" src={user.avatar_url} alt="Card image cap"/>
+  <div className="card-body">
+    <a href="#" className="btn btn-primary user-btn-clr">{`Articles by ${user.username}`}</a>
+    <a href="#" className="btn btn-primary user-btn-clr">{`Comments by ${user.username}`}</a>
+  </div>
 
+</div>
 
-            <div className="card">
-            <div className="row align-centre">{user.name}</div>
-            <div className="row align-centre">{user.username}</div>
-            <div className="row">
-            <img
-                        className=" align-centre avatar-size border border-white"
-                        src={user.avatar_url}
-                        alt="avatar"
-                      /></div>
-
-            <div className="row align-centre"> 
-            <div className="col-sm-12">
-            <div className="row align-centre"><button>{`Articles by ${user.username}`}</button></div>
-            <div className="row align-centre"><button>{`Comments by ${user.username}`}</button></div>
-            </div>
-            </div>
-            </div>
         )
     }
 }
